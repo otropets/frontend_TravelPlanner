2067fec8901dd5e28a61f11d56da18a2b3efead4
Oleksandr Tropets <245129@edu.p.lodz.pl>
2026-04-29 21:24:45 +0200

    Back to production URL

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,5 @@
-const BASE_URL = "http://localhost:8080";
-//const BASE_URL = "https://travelplanner-production-d32c.up.railway.app";
+//const BASE_URL = "http://localhost:8080";
+const BASE_URL = "https://travelplanner-production-d32c.up.railway.app";
 
 // auth
 
